44eeab3b0ceed7d5b6a2c13cdd02817894609388
Stiven Trujillo Ortiz <strujillo1293@gmail.com>
2021-01-16 16:49:06 -0500

    definiendo las citas disponibles

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -15,7 +17,7 @@ public class Doctor extends User {
     }
 
     // Comportamientos
-    public void addAvailableAppointment(Date date, String time){
+    public void addAvailableAppointment(String date, String time){
         availableAppointments.add(new Doctor.AvailableAppointment(date,time));
     }
 
@@ -41,9 +43,14 @@ public class Doctor extends User {
         private int id;
         private Date date; // Que fecha se encuentra disponible el doctor
         private String time; // hora
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 
-        public AvailableAppointment(Date date, String time) {
-            this.date = date;
+        public AvailableAppointment(String date, String time) {
+            try {
+                this.date = format.parse(date);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
             this.time = time;
         }
 
@@ -57,6 +64,10 @@ public class Doctor extends User {
 
         public Date getDate() {
             return date;
+        }
+
+        public String getDate(String DATE) {
+            return format.format(date);
         }
 
         public void setDate(Date date) {
